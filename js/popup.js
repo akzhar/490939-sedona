a34c0744924c77d00a4a8663abe8a8e5c3c8319b
akzhar <35742292+akzhar@users.noreply.github.com>
2018-03-28 20:37:33 +0300

    Картошка

--- js/popup.js
+++ js/popup.js
@@ -4,35 +4,25 @@ var datefield1 = form.querySelector("[name=search-form-sets-in]");
 var datefield2 = form.querySelector("[name=search-form-sets-out]");
 var btnSubmit = form.querySelector(".button");
 
+form.classList.remove('modal-show');
+
 btn.addEventListener("click", function(evt) {
   evt.preventDefault();
   form.classList.toggle("modal-show");
   datefield1.focus();
-  if (form.classList.contains("modal-error")) {
-    form.classList.remove("modal-error");
-  }
 });
 
-// form.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
-//   form.classList.remove("modal-error");
-//   if (!datefield1.value || !datefield2.value) {
-//     evt.preventDefault();
-//     form.classList.add("modal-error");
-//     if (!datefield1.value) {
-//       datefield1.focus();
-//       } else {
-//       datefield2.focus();
-//       }
-//   }
-// });
-
-btnSubmit.onClick = function(evt) {
-  evt.preventDefault();
+btnSubmit.onclick = function(evt) {
   form.classList.remove("modal-error");
+  form.offsetWidth = form.offsetWidth;
   if (!datefield1.value || !datefield2.value) {
     evt.preventDefault();
     form.classList.add("modal-error");
+    if (!datefield1.value) {
+      datefield1.focus();
+    } else {
+      datefield2.focus();
+    }
   }
 };
 
